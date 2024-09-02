462392de2945016d89df5243675557b39d0a75a4
sreelakshmi <sreelakshmirnathclt@gmail.com>
2024-09-02 10:44:38 +0530

    second commit

--- API/views.py
+++ API/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from API.models import Products
+
+class ProductView(APIView):
+    def get(self,request,*args,**kw):
+        return Response(data='list all products')
+    
+    def post(self,request,*args,**kw):
+        return Response(data='Item successfully added')
+
+class ProductDetailView(APIView):
+    def get(self,request,*args,**kw):
+        return Response(data='Details of a product')
+    
+    def get(self,request,*args,**kw):
+        return Response(data='Item successfully updated')
+    
+    def delete(self,request,*args,**kw):
+        return Response(data='Item deleted')
